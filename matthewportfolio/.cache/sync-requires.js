1f26e274d503fd83ada94d5de8fba535b213e247
kingtrip3 <matthew.mukherjee@georgebrown.com>
2021-03-14 23:00:41 -0400

    first commit

---
+++ matthewportfolio/.cache/sync-requires.js
@@ -0,0 +1,17 @@
+const { hot } = require("react-hot-loader/root")
+
+// prefer default export if available
+const preferDefault = m => (m && m.default) || m
+
+
+exports.components = {
+  "component---cache-dev-404-page-js": hot(preferDefault(require("/Users/matthewmukherjee/Downloads/themeforest-lBu47YRy-waxon-personal-portfolio-react-template/waxon/waxon-particles/.cache/dev-404-page.js"))),
+  "component---src-pages-404-js": hot(preferDefault(require("/Users/matthewmukherjee/Downloads/themeforest-lBu47YRy-waxon-personal-portfolio-react-template/waxon/waxon-particles/src/pages/404.js"))),
+  "component---src-pages-index-js": hot(preferDefault(require("/Users/matthewmukherjee/Downloads/themeforest-lBu47YRy-waxon-personal-portfolio-react-template/waxon/waxon-particles/src/pages/index.js"))),
+  "component---src-template-archive-js": hot(preferDefault(require("/Users/matthewmukherjee/Downloads/themeforest-lBu47YRy-waxon-personal-portfolio-react-template/waxon/waxon-particles/src/template/archive.js"))),
+  "component---src-template-blog-details-js": hot(preferDefault(require("/Users/matthewmukherjee/Downloads/themeforest-lBu47YRy-waxon-personal-portfolio-react-template/waxon/waxon-particles/src/template/blog-details.js"))),
+  "component---src-template-category-post-js": hot(preferDefault(require("/Users/matthewmukherjee/Downloads/themeforest-lBu47YRy-waxon-personal-portfolio-react-template/waxon/waxon-particles/src/template/category-post.js"))),
+  "component---src-template-project-details-js": hot(preferDefault(require("/Users/matthewmukherjee/Downloads/themeforest-lBu47YRy-waxon-personal-portfolio-react-template/waxon/waxon-particles/src/template/project-details.js"))),
+  "component---src-template-tag-template-js": hot(preferDefault(require("/Users/matthewmukherjee/Downloads/themeforest-lBu47YRy-waxon-personal-portfolio-react-template/waxon/waxon-particles/src/template/tag-template.js")))
+}
+
